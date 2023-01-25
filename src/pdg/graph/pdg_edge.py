8397d481baa3bc945d36fff4c561188c802719fa
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-01-25 20:03:32 +0100

    PDGEdge initial commit

---
+++ src/pdg/graph/pdg_edge.py
@@ -0,0 +1,29 @@
+import string
+from pdg_node import PDGNode
+import abc
+
+
+class PDGEdge:
+    __metaclass__ = abc.ABCMeta
+
+    _source: PDGNode
+    _target: PDGNode
+
+    def __init__(self, source: PDGNode, target: PDGNode):
+        self._source = source
+        self._target = target
+
+    @abc.abstractmethod
+    def get_label(self) -> string:
+        return
+
+    def get_source(self) -> PDGNode:
+        return self._source
+
+    def get_target(self) -> PDGNode:
+        return self._target
+
+    @abc.abstractmethod
+    def get_exas_label(self) -> string:
+        return
+
