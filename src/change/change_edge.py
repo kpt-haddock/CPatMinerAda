2dc4bfba009f3bb75d5071e2a195eb6a762881fb
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-01-25 20:02:01 +0100

    ChangeEdge initial commit

---
+++ src/change/change_edge.py
@@ -0,0 +1,32 @@
+import string
+
+from change_node import ChangeNode
+from src.pdg.graph.pdg_edge import PDGEdge
+
+
+class ChangeEdge:
+    source: ChangeNode
+    target: ChangeNode
+    label: string
+
+    def __init__(self, source: ChangeNode, target: ChangeNode, e: PDGEdge):
+        self.source = source
+        self.target = target
+        self.label = e.get_exas_label()
+        source.out_edges.append(self)
+        target.in_edges.append(self)
+
+    def get_source(self) -> ChangeNode:
+        return self.source
+
+    def get_target(self) -> ChangeNode:
+        return self.target
+
+    def get_label(self) -> string:
+        return self.label
+
+    def is_mapped(self) -> bool:
+        return self.label == '_map_'
+
+    def to_string(self) -> string:
+        return self.label
