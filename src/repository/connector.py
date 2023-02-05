05ca317696b2814afbec19b66c553eb396363187
Robin van Straeten <robin.van.straeten@nexperia.com>
2023-02-05 14:11:18 +0100

    Connector initial commit

---
+++ src/repository/connector.py
@@ -0,0 +1,28 @@
+import string
+from abc import ABC
+import re
+
+
+class Connector(ABC):
+    fixing_patterns: [string] = [
+        "issue[\\s]+[0-9]+",
+        "issues[\\s]+[0-9]+",
+        "issue[\\s]+#[0-9]+",
+        "issues[\\s]+#[0-9]+",
+        "issue[\\s]+# [0-9]+",
+        "bug",
+        "fix",
+        "error",
+        "exception"
+    ]
+
+    @staticmethod
+    def is_fixing_commit(commit_log: string) -> bool:
+        if commit_log is not None:
+            lower_commit_log: string = commit_log.lower()
+            for fixing_pattern in Connector.fixing_patterns:
+                if len(re.findall(fixing_pattern, lower_commit_log)) > 0:
+                    return True
+        return False
+
+
