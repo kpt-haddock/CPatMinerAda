976fa9a14a1587727cfbf60a2db45469bdaaa39d
Robin van Straeten <robin.van.straeten@nexperia.com>
2023-02-14 14:43:26 +0100

    string -> str

--- src/repository/connector.py
+++ src/repository/connector.py
@@ -1,10 +1,9 @@
-import string
 from abc import ABC
 import re
 
 
 class Connector(ABC):
-    fixing_patterns: [string] = [
+    fixing_patterns: [str] = [
         "issue[\\s]+[0-9]+",
         "issues[\\s]+[0-9]+",
         "issue[\\s]+#[0-9]+",
@@ -17,9 +16,9 @@ class Connector(ABC):
     ]
 
     @staticmethod
-    def is_fixing_commit(commit_log: string) -> bool:
+    def is_fixing_commit(commit_log: str) -> bool:
         if commit_log is not None:
-            lower_commit_log: string = commit_log.lower()
+            lower_commit_log: str = commit_log.lower()
             for fixing_pattern in Connector.fixing_patterns:
                 if len(re.findall(fixing_pattern, lower_commit_log)) > 0:
                     return True
