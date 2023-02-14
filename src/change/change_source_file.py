aa3c8948b60bf73e8fe867062cdea2ad1798b44b
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-02-14 14:36:25 +0100

    string -> str and CSourceFile -> ChangeSourceFile

--- src/change/change_source_file.py
+++ src/change/change_source_file.py
@@ -1,15 +1,12 @@
-import string
-
-
-class CSourceFile:
+class ChangeSourceFile:
     __lines_of_code: int = 0
-    __path: string
+    __path: str
 
-    def __init__(self, path: string, lines_of_code: int):
+    def __init__(self, path: str, lines_of_code: int):
         self.__lines_of_code = lines_of_code
         self.__path = path
 
-    def get_path(self) -> string:
+    def get_path(self) -> str:
         return self.__path
 
     def get_lines_of_code(self) -> int:
