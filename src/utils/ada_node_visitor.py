ebff396f5dba0f4285fd61591b6f95c0dd1cb6b9
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-03-18 12:39:10 +0100

    AdaNodeVisitor class

---
+++ src/utils/ada_node_visitor.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from libadalang import AdaNode
+
+
+def accept(node: AdaNode, visitor: AdaNodeVisitor):
+    visitor.pre_visit(node)
+    if visitor.visit(node):
+        for child in node.children:
+            if child is not None:
+                accept(child, visitor)
+    visitor.post_visit(node)
+
+
+class AdaNodeVisitor:
+
+    def pre_visit(self, node: AdaNode):
+        pass
+
+    def visit(self, node: AdaNode) -> bool:
+        print('Visitor visit: {}'.format(node))
+        return True
+
+    def post_visit(self, node: AdaNode):
+        pass
