9ec751d3044164e19ec0f8ec5f783ba0c704237d
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-06-22 09:01:29 +0200

    pdg_test updated to use unit provider

--- src/pdg/pdg_test.py
+++ src/pdg/pdg_test.py
@@ -1,13 +1,16 @@
+import time
 from typing import cast
 
-from libadalang import AnalysisContext, AdaNode, AnalysisUnit, SubpBody
+from libadalang import AnalysisContext, AdaNode, AnalysisUnit, SubpBody, GPRProject, UnitProvider
 
 from src.graphics.dot_graph import DotGraph
 from src.pdg import PDGGraph, PDGBuildingContext
 
 
 def main():
-    context = AnalysisContext()
+    project: GPRProject = GPRProject('../default.gpr')
+    provider: UnitProvider = project.create_unit_provider()
+    context = AnalysisContext(unit_provider=provider)
     unit_c: AnalysisUnit = context.get_from_file('../c.adb')
     node_c: AdaNode = unit_c.root
     for procedure in node_c.finditer(lambda n: isinstance(n, SubpBody)):
@@ -16,7 +19,8 @@ def main():
                                    PDGBuildingContext('', '', '', False))
         # graph.prune()
         dot_graph: DotGraph = DotGraph(graph, False)
-        dot_graph.to_graphics('dotfile', 'typetype')
+        dot_graph.to_graphics(procedure.p_relative_name.text, 'typetype')
+        time.sleep(1)
 
 
 if __name__ == '__main__':
