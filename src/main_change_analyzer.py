91af487f867b1566ef2d46771cd24f40e7dc4cb6
Robin van Straeten <robin.vanstraeten@ru.nl>
2023-06-22 09:02:07 +0200

    log time for analysis of ada synth lib

--- src/main_change_analyzer.py
+++ src/main_change_analyzer.py
@@ -1,33 +1,30 @@
 import os.path
 import libadalang as lal
 
+from datetime import datetime
 from src.change import ChangeAnalyzer
 
 
 class MainChangeAnalyzer:
-    context = None
-
-    def __init__(self):
-        context = lal.AnalysisContext()
 
     @staticmethod
     def analyze(path, name):
         if not os.path.isdir(path):
             return
         if os.path.exists(os.path.join(path, '.git')):
+            start_project_time = datetime.now()
             change_analyzer = ChangeAnalyzer(name, -1, path)
             change_analyzer.build_git_connector()
             change_analyzer.analyze_git()
-            print('is a git directory')
-        print(path)
-        print('Analyze')
+            end_project_time = datetime.now()
+            print('Done {} in {}'.format(name, end_project_time - start_project_time))
 
     @staticmethod
     def repositories():
         # TODO: read repositories from file.
-        return ['AdaCore/libadalang', 'AdaCore/gnatstudio']
+        return ['AdaCore/libadalang']
 
 
 if __name__ == '__main__':
     analyzer = MainChangeAnalyzer()
-    analyzer.analyze('C:/build/itecembed', 'itecembed')
+    analyzer.analyze('C:/repos/ada-synth-lib', 'C:/repos/ada-synth-lib/ada_synth_lib.gpr')
